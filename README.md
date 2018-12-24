c40904c1eb36f0e61fb1bf6219bea81a6b0df7ea
Tim Vervoort <tim.vervoort@student.uhasselt.be>
2018-12-24 16:44:47 +0100

    Encapsulated SimpleCalendar in a class, updated README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # calendar-viewer
-Google Calendar like JSON calendar viewer in browser.
+Google Calendar like JSON calendar viewer in browser. The JSON format is described in `github.com/timvervoort/ical-viewer`.
+
+# How to use
+The SimpleCalendar plugin needs jQuery and moment to function. Include the following scripts in your HTML:
+```html
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min.js"></script>
+<script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script type="text/javascript" src="js/calendar.js"></script>
+```
+
+Default styles for the calendar are present in `styles.css`.
+
+Create a DOM object with an ID. Pass the ID from that object and the url to a JSON calendar to the SimpleCalendar constructor to generate a calendar.
+```html
+<div class="calendar" id="demo"></div>
+<script type="text/javascript">
+    var calendar = new SimpleCalendar({
+        id: 'demo',
+        url: 'https://www.example.org/path/to/json/output'
+    });
+</script>
+```
+
+# TODO
+- Remove jQuery from the project.
+- Limit calendar navigation.
+- Draw current time only on the current day, even when navigating.
--- js/calendar.js
+++ js/calendar.js
@@ -1,143 +1,179 @@
-var weekdayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-var tomorrowName = 'tomorrow';
-var todayName = 'today';
-var url = 'https://www.timvervoort.com/api/agenda/';
-var beginDay = new Date().setHours(0, 0, 1, 0);
-var hours = 24;
-var nextDays = 60;
-var redrawCurrentTimeIntervalSeconds = 60;
-var currentWeek = 0;
+class SimpleCalendar {
+
+    constructor(settings) {
+
+        if (!settings.id) {
+            console.log('Error: no calendar id provided.');
+        }
+
+        if (!settings.url) {
+            console.log('Error: no calendar url provided.');
+        }
+
+        // Settings
+        this.calendarId = settings.id;
+        this.url = settings.url;
+        this.hours = (settings.hours === undefined) ? '24' : settings.hours;
+        this.nextDays = (settings.nextDays === undefined) ? '60' : settings.nextDays;
+        this.redrawTimeInt = (settings.redrawTimeInt === undefined) ? '1O' : settings.redrawTimeInt;
+        this.todayName = (settings.todayName === undefined) ? 'today' : settings.todayName;
+        this.tomorrowName = (settings.tomorrowName === undefined) ? 'tomorrow' : settings.tomorrowName;
+        this.weekdayNames = (settings.weekdayNames === undefined) ? ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'] : settings.weekdayNames;
+
+        // Helpers
+        this.beginDay = new Date().setHours(0, 0, 1, 0);  
+        this.currentWeek = 0;
+        this.dateHelper = new DateHelper();
+        this.init();
+
+    }
+
+    init() {
+        var t = this;
+        t.drawHours();
+        t.drawWeekdays();
+        t.drawDates();
+        t.addNavigationListeners();     
+        t.drawReservations();
+        t.redrawCurrent();
+        setInterval(function(){
+            t.redrawCurrent();
+        }, t.redrawTimeInt * 1000);
+    }
+
+    addNavigationListeners() {
+        var t = this;
+        document.onkeydown = function(e) {
+            switch (e.keyCode) {
+                case 37:
+                    t.prevWeek();
+                    break;
+                case 39:
+                    t.nextWeek();
+                    break;
+            }
+        };
+    }
+
+    nextWeek() {
+        this.currentWeek++;
+        document.getElementById('dates').scrollLeft = (window.innerWidth - 50) * this.currentWeek;
+    }
     
-// Returns the amount of hours between two dates
-function timeDifference(a, b) {
-    var msPerMinute = 60 * 1000;
-    var msPerHour = msPerMinute * 60;
-    var elapsed = new moment(b) - new moment(a);
-    return elapsed / msPerHour;
-}
-
-// String time helper functions
-function getHour(date) {
-    return date.split(' ')[1].split(':')[0];
-}
-function getMin(date) {
-    return date.split(' ')[1].split(':')[1];
-}
-function getTime(date) {
-    return getHour(date)+':'+getMin(date);
-}
-
-// Draw reservation
-function drawReservation(date, start, end) {
-    if (!date || !start || !end) { return; }
-    var timeDiff = timeDifference(beginDay, new Date().setHours(getHour(start), getMin(start), 0, 0));
-    var offset = Math.max((100 / hours) * timeDiff, 0);
-    var timeDiff = timeDifference(new Date().setHours(getHour(start), getMin(start), 0, 0), new Date().setHours(getHour(end), getMin(end), 0, 0));
-    var height = (100 / hours) * timeDiff;
-    var dateBlock = $('ul.dates li[data-date="'+date+'"] .reservations');
-    var reservationBlock = '<div data-start="'+start+'" data-end="'+end+'">'+getTime(start)+' - '+getTime(end)+'</div>';
-    dateBlock.append(reservationBlock);
-    var rObj = $('ul.dates li[data-date="'+date+'"] div[data-start="'+start+'"]');
-    rObj.css('top', 'calc(' + offset + '% )');
-    rObj.css('height', 'calc(' + height + '%)');
-}
-
-// Get monday for the current week
-function getMonday(d) {
-    d = new Date(d);
-    var day = d.getDay();
-    var diff = d.getDate() - day + (day == 0 ? -6:1);
-    return new Date(d.setDate(diff));
-}
-
-// Add days to Date object
-function addDays(d, qty) {
-    var newDate = new Date(d.getTime()); // Copy date
-    newDate.setDate(d.getDate() + qty);
-    return newDate;
-}
-
-// Draw next visible days
-function drawDates() {
-    var start = getMonday(new Date());
-    var d = new Date();
-    for (var i = 0; i < nextDays; i++) {
-        d = addDays(start, i);
-        var stamp = d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2);
-        document.getElementsByClassName('dates')[0].innerHTML += '<li data-date="'+stamp+'" data-day="'+d.getDay()+'"><span>'+d.getDate() + '/' + (d.getMonth() + 1) +'</span><section class="reservations"></span></li>';
+    prevWeek() {
+        this.currentWeek--;
+        document.getElementById('dates').scrollLeft = (window.innerWidth - 50) * this.currentWeek;
+    }  
+    
+    redrawCurrent() {
+        if (!document.getElementById('currentMarker')) {
+            document.getElementById(this.calendarId).innerHTML += '<div id="currentMarker"></div>';
+        }
+        var d = new Date();
+        var now = d.toString();
+        var open = new Date().setHours(0, 0, 0, 0);
+        var curDiff =  this.dateHelper.difference(open, now);
+        var curOffset = curDiff / this.hours;
+        var weekday = (d.getDay() - 1) % 7;
+        document.getElementById('currentMarker').style.top = 'calc((100% - 80px) * ' + curOffset + ' + 80px)';
+        document.getElementById('currentMarker').style.left = 'calc((100% / 7) * ' + weekday + ' + 50px)';
     }
-}
 
-// Draw weekdays
-function drawWeekdays() {
-    for (var i = 0; i < 7; i++) {
-        document.getElementsByClassName('weekdays')[0].innerHTML += '<li>'+weekdayNames[(i + 1) % 7]+'</li>';
-    }
-}
-
-// Draw hours
-function drawHours() {
-    for (var i = 0; i < hours; i++) {
-        var h = '0' + i;
-        document.getElementsByClassName('time')[0].innerHTML += '<li data-hour="'+i+'">' + h.slice(-2) + ':00</li>';
-    }
-}
-
-// Retreive JSON reservations from server
-function drawReservations() {
-    $.getJSON(url, function(data) {
-        $.each(data['data'], function(key, val) {
-            $.each(val['slots'], function(k, r) {
-                drawReservation(val['date'], r['start'], r['end']);
+    drawReservations() {
+        var t = this;
+        $.getJSON(this.url, function(data) {
+            $.each(data['data'], function(key, val) {
+                $.each(val['slots'], function(k, r) {
+                    t.drawReservation(val['date'], r['start'], r['end']);
+                });
             });
         });
-    });
-}
-
-// Redraw current time marker
-function redrawCurrentTime() {
-    var d = new Date();
-    var now = d.toString();
-    var open = new Date().setHours(0, 0, 0, 0);
-    var curDiff =  timeDifference(open, now);
-    var curTimeOffset = curDiff / hours;
-    var weekday = (d.getDay() - 1) % 7;
-    document.getElementsByClassName('currentTimeMarker')[0].style.top = 'calc((100% - 80px) * ' + curTimeOffset + ' + 80px)';
-    document.getElementsByClassName('currentTimeMarker')[0].style.left = 'calc((100% / 7) * ' + weekday + ' + 50px)';
-}
-setInterval(function(){
-    redrawCurrentTime();
-}, redrawCurrentTimeIntervalSeconds * 1000);
-
-function nextWeek() {
-    currentWeek++;
-    document.getElementsByClassName('dates')[0].scrollLeft = (window.innerWidth - 50) * currentWeek;
-}
-
-function prevWeek() {
-    currentWeek--;
-    document.getElementsByClassName('dates')[0].scrollLeft = (window.innerWidth - 50) * currentWeek;
-}
-
-// Keyboard navigation
-document.onkeydown = function(e) {
-    switch (e.keyCode) {
-        case 37:
-            prevWeek();
-            break;
-        case 39:
-            nextWeek();
-            break;
     }
-};
 
-// Init calendar
-function init() {
-    drawHours();
-    drawWeekdays();
-    drawDates();
-    drawReservations();
-    redrawCurrentTime();
+    drawHours() {
+        if (!document.getElementById('times')) {
+            document.getElementById(this.calendarId).innerHTML += '<ul id="times"></div>';
+        }
+        for (var i = 0; i < this.hours; i++) {
+            var h = '0' + i;
+            document.getElementById('times').innerHTML += '<li data-hour="'+i+'">' + h.slice(-2) + ':00</li>';
+        }
+    }
+
+    drawWeekdays() {
+        if (!document.getElementById('weekdays')) {
+            document.getElementById(this.calendarId).innerHTML += '<ul id="weekdays"></ul>';
+        }
+        for (var i = 0; i < 7; i++) {
+            document.getElementById('weekdays').innerHTML += '<li>' + this.weekdayNames[(i + 1) % 7] + '</li>';
+        }
+    }
+
+    drawDates() {
+        if (!document.getElementById('dates')) {
+            document.getElementById(this.calendarId).innerHTML += '<ul id="dates"></ul>';
+        }
+        var start = this.dateHelper.getMonday(new Date());
+        var d = new Date();
+        for (var i = 0; i < this.nextDays; i++) {
+            d = this.dateHelper.addDays(start, i);
+            var stamp = d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2);
+            document.getElementById('dates').innerHTML += '<li data-date="' + stamp + '" data-day="' + d.getDay() + '"><span>' + d.getDate() + '/' + (d.getMonth() + 1) + '</span><section class="reservations"></span></li>';
+        }
+    }
+
+    drawReservation(date, start, end) {
+        if (!date || !start || !end) { return; }
+        var d = this.dateHelper.difference(this.beginDay, new Date().setHours(this.dateHelper.getHour(start), this.dateHelper.getMin(start), 0, 0));
+        var offset = Math.max((100 / this.hours) * d, 0);
+        var d = this.dateHelper.difference(new Date().setHours(this.dateHelper.getHour(start), this.dateHelper.getMin(start), 0, 0), new Date().setHours(this.dateHelper.getHour(end), this.dateHelper.getMin(end), 0, 0));
+        var height = (100 / this.hours) * d;
+        var dateBlock = $('ul#dates li[data-date="' + date + '"] .reservations');
+        var reservationBlock = '<div data-start="' + start + '" data-end="' + end + '">' + this.dateHelper.get(start) + ' - ' + this.dateHelper.get(end) + '</div>';
+        dateBlock.append(reservationBlock);
+        var rObj = $('ul#dates li[data-date="' + date + '"] div[data-start="' + start + '"]');
+        rObj.css('top', 'calc(' + offset + '% )');
+        rObj.css('height', 'calc(' + height + '%)');
+    }
+
 }
 
-init();
+class DateHelper {
+
+    constructor() {
+
+    }
+
+    addDays(d, qty) {
+        var newDate = new Date(d.getTime()); // Copy date
+        newDate.setDate(d.getDate() + qty);
+        return newDate;
+    }
+
+    getMonday(d) {
+        d = new Date(d);
+        var day = d.getDay();
+        var diff = d.getDate() - day + (day == 0 ? -6:1);
+        return new Date(d.setDate(diff));
+    }
+
+    difference(a, b) {
+        var msPerMinute = 60 * 1000;
+        var msPerHour = msPerMinute * 60;
+        var elapsed = new moment(b) - new moment(a);
+        return elapsed / msPerHour;
+    }
+    
+    getHour(date) {
+        return date.split(' ')[1].split(':')[0];
+    }
+
+    getMin(date) {
+        return date.split(' ')[1].split(':')[1];
+    }
+
+    get(date) {
+        return this.getHour(date) + ':' + this.getMin(date);
+    }
+
+}
